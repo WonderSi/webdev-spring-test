0c3557953646faed698e7cb642617891fd3141f4
WonderSi <kirillkozlov00@gmail.com>
2026-04-21 23:06:22 +0700

    fix: add test profile to UserJpaAdapter

--- src/main/kotlin/com/example/lab5/infrastructure/jpa/adapter/UserJpaAdapter.kt
+++ src/main/kotlin/com/example/lab5/infrastructure/jpa/adapter/UserJpaAdapter.kt
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
 @Component
-@Profile("db")
+@Profile("db", "test")
 class UserJpaAdapter(
     private val userJpaRepository: UserJpaRepository
 ) : UserRepositoryPort {
